f084a926e5ddc7e5308cd7d644e176c1db320e4b
Eike Haller <58111764+eksrha@users.noreply.github.com>
2022-10-15 15:00:55 +0200

    add testing

---
+++ .github/kubeconform.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euo pipefail
+
+CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/master -- charts | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
+KUBECONFORM_VERSION="0.4.14"
+
+# install kubeconform
+curl --silent --show-error --fail --location --output /tmp/kubeconform.tar.gz https://github.com/yannh/kubeconform/releases/download/"${KUBECONFORM}"/kubeconform-linux-amd64.tar.gz 
+tar -xf /tmp/kubeconform.tar.gz kubeconform
+
+# validate charts
+for CHART_DIR in ${CHART_DIRS}; do
+  helm template "${CHART_DIR}" | ./kubeconform -strict -ignore-missing-schemas -kubernetes-version "${KUBERNETES_VERSION#v}"
+done
